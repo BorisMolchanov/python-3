63ce34b4bd80a88367f01a8712a54846ced6e39a
steel-chain <99689226+steel-chain@users.noreply.github.com>
2022-06-14 11:18:40 +0300

    Create binary_search.py

---
+++ Algorithms/binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(list, item):
+	low = 0
+	high = len(list)-1
+
+	while low <= high:
+		mid = (low + high)//2
+		guess = list[mid]
+		if guess == item:
+			return mid
+		if guess > item:
+			high = mid - 1
+		else:
+			low = mid + 1
+	return None
+
+my_list = [1,3,5,7,9, 11, 12, 15]
+
+print(binary_search(my_list, 7)) # = 3, corresponds to the 4th position
+print(binary_search(my_list, 14)) # = None, no match found
