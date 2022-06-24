ec3febb0bb07f1e1a82557819b70a36c3d92124a
steel-chain <99689226+steel-chain@users.noreply.github.com>
2022-06-24 23:23:12 +0300

    Update task5.py

--- Euler_project/task5.py
+++ Euler_project/task5.py
@@ -1,8 +1,8 @@
 x = int(input("Enter first number: ")) 
-y = int(input("Enter second number: "))
-n = 20 # ищем наименьшее общее кратное для всех чисел от 1 до n
+y = x+1
+n = int(input("Enter last number: ")) # ищем наименьшее общее кратное для всех чисел от 1 до n
 
-def calculate_hcf(x, y): # ищем наибольший общий делитель (НОД)
+def calculate_hcf(x, y):
     if x > y: 
         smaller = y 
     else: 
@@ -12,19 +12,13 @@ def calculate_hcf(x, y): # ищем наибольший общий делите
             hcf = i
     return hcf
 
-def calculate_lcm(x,y): # ищем наименьшее общее кратное НОК по формуле НОК(a,b) = a*b/НОД(ab)
-    lcm = x*y/calculate_hcf(x,y)
+def calculate_lcm(x,y):
+    lcm = x*y//calculate_hcf(x,y)
     return lcm
 
-# попарно прогоняем по циклу 1е и 2е число, получаем НОК(1,2) далее НОК(1,2) и 3е число и т д.
-while y<n: 
+
+while y<n:
     x = calculate_lcm(x,y)
     y +=1
 
-print(calculate_lcm(x, y))
-
-
-
-
-print("The H.C.F. of", x,"and", y,"is", calculate_hcf(x, y))
-print("The L.C.M. of", x,"and", y,"is", calculate_lcm(x, y))
+print("The L.C.M. is", calculate_lcm(x, y))
